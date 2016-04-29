87f8e7d24d7f89c2a7a538a6600d68e5ba5dd85f
asawarik@andrew.cmu.edu <asawarik@andrew.cmu.edu>
2016-04-28 20:24:45 -0400

    after recovery branch stuff

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ActiveRecord::Base
     belongs_to :category
 
-    validate_presence_of :title
-    validate_presence_of :content
+    validates_presence_of :title
+    validates_presence_of :content
 
     scope :alphabetical, order('title')
     scope :active, where('active = ?', true)
